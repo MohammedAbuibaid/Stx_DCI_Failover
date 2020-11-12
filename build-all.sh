6f2ac811cdb7e358f61208f61fb1b351ac46fa75
Thomas Yungblut <sifotes@hotmail.com>
2020-11-12 14:20:43 -0500

    Added a script to build all required docker images locally

---
+++ build-all.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+echo $(pwd)
+docker build ./TempMonWeb/. -t 4901-capstone2020/tempmonweb
+
+sleep 2
+docker build ./TempMonSensor/. -t 4901-capstone2020/tempmonsensor
+
+echo Done building.
+docker images
