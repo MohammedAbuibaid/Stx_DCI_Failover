d54ef0d09e2ac7c2c6fdcda1aede628d02898193
Thomas Yungblut <26532668+sifotes@users.noreply.github.com>
2021-03-02 15:22:11 -0800

    Updated build script

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo $(pwd)
 
-docker build ./TempMonWeb/. -t sifotes/4901capstone-tempmonweb
+docker build ./TempMonWeb/. -t sifotes/4901capstone-tempmonweb:demo --no-cache
 
 sleep 2
-docker build ./TempMonSensor/. -t sifotes/4901capstone-tempmonsensor
+docker build ./TempMonSensor/. -t sifotes/4901capstone-tempmonsensor:demo --no-cache
+
+sleep 2
+docker build ./TempMonDB/. -t sifotes/4901capstone-tempmondb:demo --no-cache
+
 
 echo Done building.
 
 docker images
-docker login && docker push sifotes/4901capstone-tempmonweb
-docker login && docker push sifotes/4901capstone-tempmonsensor
+docker login && docker push sifotes/4901capstone-tempmonweb:demo
+docker login && docker push sifotes/4901capstone-tempmonsensor:demo
+docker login && docker push sifotes/4901capstone-tempmondb:demo
